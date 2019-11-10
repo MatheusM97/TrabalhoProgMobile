23e54637eb5b367137e87d632285492148c09353
matheus97 <matheusademelo@gmail.com>
2019-11-09 21:36:24 -0300

    Implementação da Activity do contato da mensagem, da classe user e demais xml

--- Chat_Mobile/app/src/main/java/com/example/chat_mobile/User.java
+++ Chat_Mobile/app/src/main/java/com/example/chat_mobile/User.java
@@ -4,13 +4,21 @@ package com.example.chat_mobile;
 //Clase User que é utilizada para gerenciar os dados do usuário no banco de dados do
 //firestore
 
-public class User {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
 
 
     //Variaveis do usuário
-    private final String uuid;
-    private  final String username;
-    private final String profileUrl;
+    private  String uuid;
+    private  String username;
+    private  String profileUrl;
+
+
+    public User(){
+
+    }
 
     //Contrutor do usuário
     public User(String uuid, String username, String profileUrl) {
@@ -18,6 +26,24 @@ public class User {
         this.username = username;
         this.profileUrl = profileUrl;
     }
+
+    protected User(Parcel in) {
+        uuid = in.readString();
+        username = in.readString();
+        profileUrl = in.readString();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 
     public String getUuid() {
         return uuid;
@@ -29,5 +55,17 @@ public class User {
 
     public String getProfileUrl() {
         return profileUrl;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(uuid);
+        dest.writeString(username);
+        dest.writeString(profileUrl);
     }
 }
