91b9de73c9f3e111893eb979440f6f091a6fc0dd
matheus97 <matheusademelo@gmail.com>
2019-11-25 23:24:00 -0300

    Correção orientação das fotos

--- Chat_Mobile/app/src/main/java/com/example/chat_mobile/EditarUsuario.java
+++ Chat_Mobile/app/src/main/java/com/example/chat_mobile/EditarUsuario.java
@@ -2,7 +2,10 @@ package com.example.chat_mobile;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -36,6 +39,7 @@ import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -125,7 +129,7 @@ public class EditarUsuario extends AppCompatActivity {
         //caso a foto venha da camesa
         if (requestCode == 1 && resultCode == RESULT_OK) {
             //pegando o caminho e extraindo a url da foto tirada pela camera
-            File f = new File(currentPhotoPath);
+            File f = new File(getRightAngleImage(currentPhotoPath));
             mSelectUri = Uri.fromFile(f);
             //System.out.println(mSelectUri);
             //mSelectUri = extras.get("data");
@@ -388,5 +392,77 @@ public class EditarUsuario extends AppCompatActivity {
     public void cancel(View view) {
         Intent intente = new Intent(EditarUsuario.this,MensagensActivity.class);
         startActivity(intente);
+    }
+
+    //corrigindo angulo da foto
+    private String getRightAngleImage(String photoPath) {
+
+        try {
+            ExifInterface ei = new ExifInterface(photoPath);
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            int degree = 0;
+
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_NORMAL:
+                    degree = 0;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+                case ExifInterface.ORIENTATION_UNDEFINED:
+                    degree = 0;
+                    break;
+                default:
+                    degree = 90;
+            }
+
+            return rotateImage(degree,photoPath);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return photoPath;
+    }
+
+    private String rotateImage(int degree, String imagePath){
+
+        if(degree<=0){
+            return imagePath;
+        }
+        try{
+            Bitmap b= BitmapFactory.decodeFile(imagePath);
+
+            Matrix matrix = new Matrix();
+            if(b.getWidth()>b.getHeight()){
+                matrix.setRotate(degree);
+                b = Bitmap.createBitmap(b, 0, 0, b.getWidth(), b.getHeight(),
+                        matrix, true);
+            }
+
+            FileOutputStream fOut = new FileOutputStream(imagePath);
+            String imageName = imagePath.substring(imagePath.lastIndexOf("/") + 1);
+            String imageType = imageName.substring(imageName.lastIndexOf(".") + 1);
+
+            FileOutputStream out = new FileOutputStream(imagePath);
+            if (imageType.equalsIgnoreCase("png")) {
+                b.compress(Bitmap.CompressFormat.PNG, 100, out);
+            }else if (imageType.equalsIgnoreCase("jpeg")|| imageType.equalsIgnoreCase("jpg")) {
+                b.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            }
+            fOut.flush();
+            fOut.close();
+
+            b.recycle();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return imagePath;
     }
 }
